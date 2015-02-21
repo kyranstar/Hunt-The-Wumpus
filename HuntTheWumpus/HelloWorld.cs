49eff2d65a5c17da3298dd23b85e888412bb62b6
Kyran Adams <kyranstar@yahoo.com>
2015-02-20 17:23:44 -0800

    Initial commit

---
+++ HuntTheWumpus/HelloWorld.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HuntTheWumpus
+{
+    class HelloWorld
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello World!");
+
+            // Keep the console window open in debug mode.
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
