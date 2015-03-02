1795dcd7646279a9b4b3f2298ff2e2ecc3bb2e52
WasabiFan <wasabifan@outlook.com>
2015-03-02 11:12:57 -0800

    Add modern Windows app projects

---
+++ Final Game/Platform/ModernWindows/ModernWindows/ModernWindows.Shared/GamePage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using MonoGame.Framework;
+using HuntTheWumpus.GameCore;
+
+namespace HuntTheWumpus.Platform.ModernWindows
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class GamePage : SwapChainBackgroundPanel
+    {
+        readonly GameHost _game;
+
+        public GamePage(string launchArguments)
+        {
+            this.InitializeComponent();
+
+            // Create the game.
+
+            _game = XamlGame<GameHost>.Create(launchArguments, Window.Current.CoreWindow, this);
+        }
+    }
+}
