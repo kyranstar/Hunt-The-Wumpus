dda0b9dab79988557d056d04450ec518b482c89a
WasabiFan <wasabifan@outlook.com>
2015-03-23 10:17:00 -0700

    Fix small error in scene interface

--- Final Game/Shared Code/Game Control/IScene.cs
+++ Final Game/Shared Code/Game Control/IScene.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -14,30 +16,30 @@ namespace HuntTheWumpus.SharedCode.GameControl
         /// Called every time that the scene is loaded. This will be called before Update() or Draw.
         /// If the scene stores state, this is probably the place to reset it.
         /// </summary>
-        public void Initialize();
+        void Initialize();
 
         /// <summary>
         /// Will be called once per game execution, when the program is started. This is where you load
         /// media assets.
         /// </summary>
-        public void LoadContent();
+        void LoadContent();
 
         /// <summary>
         /// Will be called once per game execution, while the program is closing. This is where you should unload assets,
         /// save special content, etc.
         /// </summary>
-        public void UnloadContent();
+        void UnloadContent();
 
         /// <summary>
         /// Will be called repeatedly as long as this scene is loaded. This is where you should update game state and
         /// handle user input to prepare for rendering.
         /// </summary>
-        public void Update(GameTime GameTime);
+        void Update(GameTime GameTime);
 
         /// <summary>
         /// This will be called every time a frame is drawn. You should draw everything here using the given
         /// <code>SpriteBatch</code>.
         /// </summary>
-        public void Draw(GameTime GameTime, SpriteBatch TargetBatch);
+        void Draw(GameTime GameTime, SpriteBatch TargetBatch);
     }
 }
