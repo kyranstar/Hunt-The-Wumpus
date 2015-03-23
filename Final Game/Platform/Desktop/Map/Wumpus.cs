8aa4609a282c5ce48aa431b080e7fda090eb14c2
Kyran Adams <kyranstar@yahoo.com>
2015-03-23 16:10:01 -0700

    Added Map and Wumpus classes

---
+++ Final Game/Platform/Desktop/Map/Wumpus.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HuntTheWumpus.Platform.Desktop.Map
+{
+    class Wumpus
+    {
+        private int x, y;
+        private Cave cave;
+
+        public Wumpus(Cave cave)
+        {
+            this.cave = cave;
+        }
+        /// <summary>
+        /// Moves the wumpus to a nearby valid position
+        /// </summary>
+        public void Move()
+        {
+
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns>The position of the wumpus</returns>
+        public Point GetPosition()
+        {
+            return new Point(x, y);
+        }
+    }
+}
