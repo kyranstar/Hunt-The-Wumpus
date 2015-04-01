363775be3745c90925064cff4529b994a5712efb
WasabiFan <wasabifan@outlook.com>
2015-03-31 19:30:57 -0700

    Add blank CaveRenderer class

---
+++ Final Game/Shared Code/GUI/CaveRenderer.cs
@@ -0,0 +1,14 @@
+﻿using HuntTheWumpus.SharedCode.GameControl;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HuntTheWumpus.SharedCode.GUI
+{
+    class CaveRenderer
+    {
+        // TODO: Implement
+    }
+}
