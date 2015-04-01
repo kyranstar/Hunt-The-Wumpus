811bc3327f6078d8530579d852efcfc2ec03af1c
WasabiFan <wasabifan@outlook.com>
2015-03-31 19:29:23 -0700

    Add initial code to lay-out cave (currently uses fake data)

--- Final Game/Shared Code/Game Control/Logger.cs
+++ Final Game/Shared Code/Game Control/Logger.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 
@@ -7,52 +8,49 @@ namespace HuntTheWumpus.SharedCode.GameControl
 {
     static class Log
     {
-#if !NETFX_CORE
         public static void Info(string Message)
         {
-            WriteWithColor("INFO:", ConsoleColor.White, ConsoleColor.DarkCyan);
-            WriteLineWithColor(" " + Message, ConsoleColor.Cyan, ConsoleColor.Black);
+#if !NETFX_CORE
+            WriteToConsole("INFO:", ConsoleColor.White, ConsoleColor.DarkCyan);
+            WriteLineToConsole(" " + Message, ConsoleColor.Cyan, ConsoleColor.Black);
+#endif
+            Debug.WriteLine("INFO: " + Message);
         }
 
         public static void Warn(string Message)
         {
-            WriteWithColor("WARN:", ConsoleColor.White, ConsoleColor.DarkYellow);
-            WriteLineWithColor(" " + Message, ConsoleColor.Yellow, ConsoleColor.Black);
+#if !NETFX_CORE
+            WriteToConsole("WARN:", ConsoleColor.White, ConsoleColor.DarkYellow);
+            WriteLineToConsole(" " + Message, ConsoleColor.Yellow, ConsoleColor.Black);
+#endif
+            Debug.WriteLine("WARN: " + Message);
         }
 
         public static void Error(string Message)
         {
-            WriteWithColor("ERR:", ConsoleColor.White, ConsoleColor.DarkRed);
-            WriteLineWithColor(" " + Message, ConsoleColor.Red, ConsoleColor.Black);
+#if !NETFX_CORE
+            WriteToConsole("ERR:", ConsoleColor.White, ConsoleColor.DarkRed);
+            WriteLineToConsole(" " + Message, ConsoleColor.Red, ConsoleColor.Black);
+#endif
+            Debug.WriteLine("ERR: " + Message);
         }
 
-        private static void WriteLineWithColor(string Message, ConsoleColor Foreground, ConsoleColor Background)
+        
+#if !NETFX_CORE
+        private static void WriteLineToConsole(string Message, ConsoleColor Foreground, ConsoleColor Background)
         {
             Console.ForegroundColor = Foreground;
             Console.BackgroundColor = Background;
             Console.WriteLine(Message);
+
+            Debug.WriteLine(Message);
         }
 
-        private static void WriteWithColor(string Message, ConsoleColor Foreground, ConsoleColor Background)
+        private static void WriteToConsole(string Message, ConsoleColor Foreground, ConsoleColor Background)
         {
             Console.ForegroundColor = Foreground;
             Console.BackgroundColor = Background;
             Console.Write(Message);
-        }
-#else
-        public static void Info(string Message)
-        {
-            // TODO: Log in modern environment
-        }
-
-        public static void Warn(string Message)
-        {
-            // TODO: Log in modern environment
-        }
-
-        public static void Error(string Message)
-        {
-            // TODO: Log in modern environment
         }
 #endif
     }
--- Final Game/Shared Code/Scenes/GameScene.cs
+++ Final Game/Shared Code/Scenes/GameScene.cs
@@ -7,9 +7,11 @@ using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 
 using HuntTheWumpus.SharedCode;
+using HuntTheWumpus.SharedCode.Helpers;
 
 using EmptyKeys.UserInterface.Mvvm;
 using EmptyKeys.UserInterface.Input;
+using HuntTheWumpus.SharedCode.GameControl;
 
 namespace HuntTheWumpus.SharedCode.Scenes
 {
@@ -34,6 +36,86 @@ namespace HuntTheWumpus.SharedCode.Scenes
 
         public override void Draw(GameTime GameTime, SpriteBatch TargetBatch)
         {
+            //TODO: Refactor most drawing code to somewhere else
+
+            // Should use Cave.GetCave() instead when it is implemented and exposed
+            Room[] Rooms = new Room[]
+            {
+                new Room()
+                {
+                    number = 0,
+                    // Using a "square" pattern for now to simplify initial test code
+                    connections = new int[] {-1, 1, -1, -1}
+                },
+                new Room()
+                {
+                    number = 1,
+                    connections = new int[] {2, -1, -1, 0}
+                },
+                new Room()
+                {
+                    number = 2,
+                    connections = new int[] {-1, -1, 1, -1}
+                }
+            };
+
+            Room CurrentRoom = Rooms[0];
+            var R = GetRoomLayout(CurrentRoom, new Vector2(), Rooms.ToDictionary(Room => Room.number));
+
+        }
+
+        /// <summary>
+        /// Lays out the given map by converting the room's individual connections into absolute positions
+        /// </summary>
+        /// <param name="CurrentRoom">The room to start at</param>
+        /// <param name="CurrentPoint">The position that the current room should be at</param>
+        /// <param name="UnmappedRooms">The list of the unmapped rooms, indexed by ID</param>
+        /// <returns>A mapping of room IDs to their room's positions</returns>
+        private Dictionary<int, Vector2> GetRoomLayout(Room CurrentRoom, Vector2 CurrentPoint, Dictionary<int, Room> UnmappedRooms)
+        {
+            Log.Info("GetRoomLayout called for room " + CurrentRoom.number + " at point " + CurrentPoint + " with " + UnmappedRooms.Count + " unmapped rooms");
+
+            // Start with an empty result
+            Dictionary<int, Vector2> NewMappedRooms = new Dictionary<int, Vector2>();
+
+            // Iterate over the connections (the index in the array indicates the side)
+            for(int ConnectionDirection = 0; ConnectionDirection < CurrentRoom.connections.Length; ConnectionDirection++)
+            {
+                // Get the ID of the current room
+                int ConnectedRoomID = CurrentRoom.connections[ConnectionDirection];
+
+                Room NextRoom;
+                // Only process the room if it hasn't been processed already
+                if(UnmappedRooms.TryGetValue(ConnectedRoomID, out NextRoom))
+                {
+                    // Get the point for the next room
+                    Vector2 NextPoint = CurrentPoint + GetOffsetForSide(ConnectionDirection, 20f);
+                    // Remove the room now that we have calculated its position
+                    //   we don't want the next call to index it again
+                    UnmappedRooms.Remove(ConnectedRoomID);
+
+                    // Recurse through the connections of the next room
+                    Dictionary<int, Vector2> MappedRooms = GetRoomLayout(NextRoom, NextPoint, UnmappedRooms);
+
+                    // Add the current room to the deeper map
+                    MappedRooms.Add(NextRoom.number, NextPoint);
+                    // Merge the result with the results from the other connections
+                    NewMappedRooms = NewMappedRooms.MergeLeft(MappedRooms);
+                    
+                }
+            }
+
+            return NewMappedRooms;
+        }
+
+        private Vector2 GetOffsetForSide(int Side, float Apothem)
+        {
+            // TODO: Look into managing floating-point inaccuracies
+            // Assuming 'North' is side 0
+            double Angle = -(Math.PI / 2) * Side + (Math.PI / 2);
+            return new Vector2(
+                (float)Math.Cos(Angle) * Apothem * 2f,
+                (float)Math.Sin(Angle) * Apothem * 2f);
         }
 
         public override void UnloadContent()
