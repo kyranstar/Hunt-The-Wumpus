737fe73eb93c5b8eebf13cc0f3176689f9518f37
Kyran Adams <kyranstar@yahoo.com>
2015-04-01 15:50:20 -0700

    Added testing project and I think(?) moved MapGenerator to SharedCode.

---
+++ Final Game/Test/HuntTheWumpusTests/Map/MapTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HuntTheWumpusTests
+{
+    [TestClass]
+    public class Map
+    {
+        [TestMethod]
+        public void MapCreationTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
