2013bc5af78eb200ca3e8bb08e157c9847c4332d
WasabiFan <wasabifan@outlook.com>
2015-04-01 10:15:01 -0700

    Add better logging to find errors in map generation

--- Final Game/Shared Code/GUI/MapRenderer.cs
+++ Final Game/Shared Code/GUI/MapRenderer.cs
@@ -48,13 +48,28 @@ namespace HuntTheWumpus.SharedCode.GUI
                 new Room()
                 {
                     number = 1,
-                    connections = new int[] {2, -1, -1, 0}
+                    connections = new int[] {2, 3, -1, 0}
                 },
                 new Room()
                 {
                     number = 2,
                     connections = new int[] {-1, -1, 1, -1}
-                }
+                },
+                new Room()
+                {
+                    number = 3,
+                    connections = new int[] {-1, 4, -1, 1}
+                },
+                new Room()
+                {
+                    number = 4,
+                    connections = new int[] {-1, 5, -1, 3}
+                },
+                new Room()
+                {
+                    number = 5,
+                    connections = new int[] {-1, -1, -1, 4}
+                },
             };
 
             RoomLayout = GetRoomLayout(Rooms);
@@ -68,7 +83,7 @@ namespace HuntTheWumpus.SharedCode.GUI
         public void DrawCaveBase(SpriteBatch Target)
         {
             if(RoomBaseTexture == null || RoomLayout == null)
-                throw new Exception("Textures and cave layout must be loaded before the cave can be drawn.");
+                Log.Error("Textures and cave layout must be loaded before the cave can be drawn.");
 
             foreach(KeyValuePair<int, Vector2> LayoutMapping in RoomLayout)
             {
@@ -87,7 +102,14 @@ namespace HuntTheWumpus.SharedCode.GUI
         /// <returns>A mapping of room IDs to their room's positions</returns>
         private Dictionary<int, Vector2> GetRoomLayout(Room[] Rooms)
         {
-            return GetRoomLayout(Rooms[0], new Vector2(), Rooms.ToDictionary(Room => Room.number));
+            Dictionary<int, Room> UnmappedRooms = Rooms.ToDictionary(Room => Room.number);
+            Dictionary<int, Vector2> NewLayout = GetRoomLayout(Rooms[0], new Vector2(), UnmappedRooms);
+
+            // If not all rooms were found, we know that not all of them have a valid connection
+            if(UnmappedRooms.Count > 0)
+                Log.Warn("Some rooms have no valid connections and have not been positioned! The following rooms haven't been connected: " + String.Join(", ", UnmappedRooms.Keys.ToArray()));
+
+            return NewLayout;
         }
 
         /// <summary>
@@ -114,6 +136,11 @@ namespace HuntTheWumpus.SharedCode.GUI
                 // Only process the room if it hasn't been processed already
                 if (UnmappedRooms.TryGetValue(ConnectedRoomID, out NextRoom))
                 {
+                    // If we have gotten to this next room by reference but the next room
+                    //   does not have a connection back to the first one, warn of issues!
+                    if(!NextRoom.connections.Contains(CurrentRoom.number))
+                        Log.Warn("Room " + CurrentRoom.number + " claims it is connected to room " + NextRoom.number + ", but the inverse connection was not found!");
+
                     // Get the point for the next room
                     Vector2 NextPoint = CurrentPoint + GetOffsetForSide(ConnectionDirection, 20f);
                     // Remove the room now that we have calculated its position
