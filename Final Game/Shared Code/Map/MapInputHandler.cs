5afa6674d2fdee7cbf94de1b67476e90f56b004b
Kyran Adams <kyranstar@yahoo.com>
2015-04-03 21:54:24 -0700

    Added the actual file

---
+++ Final Game/Shared Code/Map/MapInputHandler.cs
@@ -0,0 +1,37 @@
+﻿using HuntTheWumpus.SharedCode.GameMap;
+using HuntTheWumpus.SharedCode.GameControl;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HuntTheWumpus.SharedCode.GameMap
+{
+    class MapInputHandler
+    {
+        private Map map;
+        private Keys[] keys = {};
+
+        public MapInputHandler(Map map)
+        {
+            this.map = map;
+        }
+
+        public void Update(GameTime time)
+        {
+            foreach(Keys key in Keyboard.GetState().GetPressedKeys())
+            {
+                if (!keys.Contains(key))
+                {
+                    //new key is pressed
+                    Log.Info("Key pressed: " + key);
+                }
+            }
+            
+            keys = Keyboard.GetState().GetPressedKeys();
+        }
+    }
+}
