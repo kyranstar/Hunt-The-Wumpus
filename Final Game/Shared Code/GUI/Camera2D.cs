435259be5dc3c1f15eb4e59cda309645fa509e75
Wasabi Fan <wasabifan@outlook.com>
2015-04-18 20:04:09 -0700

    Make renderer scale based on screen resolution

--- Final Game/Shared Code/GUI/Camera2D.cs
+++ Final Game/Shared Code/GUI/Camera2D.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,19 +10,33 @@ namespace HuntTheWumpus.SharedCode.GUI
 {
     public class Camera2D
     {
-        public Camera2D()
+        public float Zoom { get; set; }
+        public Vector2 Position { get; set; }
+        public float Rotation { get; set; }
+        public Vector2 Origin { get; set; }
+
+        /// <summary>
+        /// The camera's (virtual) viewport size
+        /// </summary>
+        public Vector2? VirtualViewSize { get; set; }
+
+        /// <summary>
+        /// The graphics device's viewport
+        /// </summary>
+        public Viewport? RenderViewport { get; set; }
+
+
+        public Camera2D(Vector2? VirtualViewSize = null, Viewport? RenderViewport = null)
         {
             Zoom = 1;
             Position = Vector2.Zero;
             Rotation = 0;
             Origin = Vector2.Zero;
             Position = Vector2.Zero;
-        }
 
-        public float Zoom { get; set; }
-        public Vector2 Position { get; set; }
-        public float Rotation { get; set; }
-        public Vector2 Origin { get; set; }
+            this.VirtualViewSize = VirtualViewSize;
+            this.RenderViewport = RenderViewport;
+        }
 
         public void Move(Vector2 direction)
         {
@@ -30,12 +45,22 @@ namespace HuntTheWumpus.SharedCode.GUI
 
         public Matrix GetTransform()
         {
-            var translationMatrix = Matrix.CreateTranslation(new Vector3(Position.X, Position.Y, 0));
-            var rotationMatrix = Matrix.CreateRotationZ(Rotation);
-            var scaleMatrix = Matrix.CreateScale(new Vector3(Zoom, Zoom, 1));
-            var originMatrix = Matrix.CreateTranslation(new Vector3(Origin.X, Origin.Y, 0));
+            Matrix TranslationMatrix = Matrix.CreateTranslation(new Vector3(Position.X, Position.Y, 0));
+            Matrix RotationMatrix = Matrix.CreateRotationZ(Rotation);
+            Matrix ZoomMatrix = Matrix.CreateScale(new Vector3(Zoom, Zoom, 1));
+            Matrix OriginMatrix = Matrix.CreateTranslation(new Vector3(Origin.X, Origin.Y, 0));
 
-            return translationMatrix * rotationMatrix * scaleMatrix * originMatrix;
+            Matrix VirtualTransform = TranslationMatrix * RotationMatrix * ZoomMatrix * OriginMatrix;
+
+            if(!RenderViewport.HasValue || !VirtualViewSize.HasValue)
+                return VirtualTransform;
+
+            Matrix RenderScaleMatrix = Matrix.CreateScale( new Vector3(
+                RenderViewport.Value.Width / VirtualViewSize.Value.X,
+                RenderViewport.Value.Height / VirtualViewSize.Value.Y,
+                1f));
+
+            return VirtualTransform * RenderScaleMatrix;
         }
     }
 }
