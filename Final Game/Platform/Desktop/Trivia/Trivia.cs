eb34f88fd4c06920765927f3992aa7242cedecb3
Sonia <sonia.skarbek@gmail.com>
2015-04-23 10:49:10 -0700

    Started trivia

---
+++ Final Game/Platform/Desktop/Trivia/Trivia.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HuntTheWumpus.Platform.Desktop.Trivia
+{
+    class Trivia
+    {
+    }
+}
