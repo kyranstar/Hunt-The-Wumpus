e008d0841041f6ca631447025c9883ad7c879dcb
Sonia <sonia.skarbek@gmail.com>
2015-04-23 18:50:46 -0700

    Started trivia class

--- Final Game/Platform/Desktop/Trivia/Trivia.cs
+++ Final Game/Platform/Desktop/Trivia/Trivia.cs
@@ -4,9 +4,24 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using HuntTheWumpus.SharedCode.GameControl;
+
 namespace HuntTheWumpus.Platform.Desktop.Trivia
 {
     class Trivia
     {
+        private List<String> _hints = new List<String>();
+
+        public List<String> Hints
+        {
+            get { return _hints; }
+        }
+        
+        public Trivia()
+        {
+            Log.Info("Creating Trivia...");
+            _hints.AddRange(new string[] {"hint1", "hint2", "hint3"});
+
+        }
     }
 }
