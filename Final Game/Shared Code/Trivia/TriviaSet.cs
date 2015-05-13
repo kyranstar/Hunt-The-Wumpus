d60ad7fb6574c9a7477696aa5e0b3c5b8208c955
Sonia <sonia.skarbek@gmail.com>
2015-05-13 14:57:32 -0700

    added trivia set

---
+++ Final Game/Shared Code/Trivia/TriviaSet.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HuntTheWumpus.SharedCode.Trivia
+{
+    class TriviaSet
+    {
+        private int _counter;
+
+        public int Counter
+        {
+            get { return _counter; }
+        }
+        private List<Question> _qlist;
+        public List<Question> QList
+        {
+            get { return _qlist; }
+        }
+
+        public TriviaSet(List<Question> QList)
+        {
+            _qlist = QList;
+        }
+    }
+}
