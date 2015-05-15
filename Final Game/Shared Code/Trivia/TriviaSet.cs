f304826209eff4ba44bfbad9935922c3b9efa0ad
Wasabi Fan <wasabifan@outlook.com>
2015-05-14 21:34:03 -0700

    Small tracker changes to trivia set class

--- Final Game/Shared Code/Trivia/TriviaSet.cs
+++ Final Game/Shared Code/Trivia/TriviaSet.cs
@@ -8,15 +8,15 @@ namespace HuntTheWumpus.SharedCode.Trivia
 {
     public class TriviaSet
     {
-        private int _questionCounter;
+        private int _currentQuestionIndex;
         private int _numberCorrect;
         public int NumberCorrect
         {
-            get{return _numberCorrect;}
+            get{ return _numberCorrect; }
         }
         public int QuestionCounter
         {
-            get { return _questionCounter; }
+            get { return _currentQuestionIndex; }
         }
         private List<Question> _qlist;
         public List<Question> QList
@@ -24,16 +24,42 @@ namespace HuntTheWumpus.SharedCode.Trivia
             get { return _qlist; }
         }
 
+        public Question CurrentQuestion
+        {
+            get
+            {
+                if (IsComplete)
+                    return null;
+
+                return _qlist[_currentQuestionIndex];
+            }
+        }
+
+        public bool IsComplete
+        {
+            get
+            {
+                return _currentQuestionIndex < 0 || _currentQuestionIndex >= _qlist.Count;
+            }
+        }
+
         public TriviaSet(List<Question> QList)
         {
             _qlist = QList;
         }
-        public void IsQuestionCorrect(Question question, String PlayerAnswer)
+
+        public bool SubmitAnswer(string PlayerAnswer)
         {
-            if (question.IsCorrect(PlayerAnswer))
-            {
+            if (IsComplete)
+                // TODO: What do we want to do here? Exception?
+                return false;
+
+            bool Correct = CurrentQuestion.IsCorrect(PlayerAnswer);
+            if (Correct)
                 _numberCorrect++;
-            }
+
+            _currentQuestionIndex++;
+            return Correct;
         }
     }
 }
